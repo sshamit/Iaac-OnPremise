73bd9277ee66b50daa60d6b4730c0664cec241bb
amitjattan <amitjattan@gmail.com>
2018-08-22 15:44:55 +0530

    testing

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -8,12 +8,11 @@ apt-add-repository ppa:ansible/ansible
 # Install Ansible.
 apt -y update
 apt -y install ansible
-
-
 apt-get install -y unzip
-cd /usr/local/bin
-wget https://releases.hashicorp.com/consul/1.2.0/consul_1.2.0_linux_amd64.zip
-unzip *.zip
-rm *.zip
-cd ~
-consul --version
+ansible-playbook ../ansible/apache.yml
+#cd /usr/local/bin
+#wget https://releases.hashicorp.com/consul/1.2.0/consul_1.2.0_linux_amd64.zip
+#unzip *.zip
+#rm *.zip
+#cd ~
+#consul --version
